8dfe5f0c340927eb3bb3fe077583b07594219a31
Egor <egor.bulavintsev@simbirsoft.com>
2025-03-20 15:50:34 +0300

    hhhhhhhhhhhhhh

--- src/eclipseFirstProject/HelloWorld.java
+++ src/eclipseFirstProject/HelloWorld.java
@@ -3,7 +3,9 @@ package eclipseFirstProject;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		for(int i = 0; i < 100; i++) {
+		System.out.println("hello world " + i);
+		}
 	}
 
 }
